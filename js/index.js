995bce4dfff505b1feaa6b44293a3b64e13becba
meryem lahrach <lahrachemeriem18@gmail.com>
2020-10-29 19:00:27 +0100

    add tooltip

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-
-
+window.addEventListener('load', function() {
+    console.log('All assets are loaded')
+    display_stars();
+})
     function dis(btn) {
         if(document.getElementById(btn).style.display =="block"){
             document.getElementById(btn).style.display ="none";
@@ -7,27 +9,29 @@
             document.getElementById(btn).style.display ="block";
         }
     }
-function hide(btn1,btn2,btn3,btn4,btn5) {
-    document.getElementById(btn1).style.display ="none";
-    document.getElementById(btn2).style.display ="none";
-    document.getElementById(btn3).style.display ="none";
-    document.getElementById(btn4).style.display ="none";
-    document.getElementById(btn5).style.display ="none";
-}
-// function myFunction() {
-//
-//     if(document.getElementById('text').style.display =="block"){
-//         document.getElementById("text").style.display ="none";
-//     }else{
-//         document.getElementById('text').style.display ="block";
-//     }
-// }
-// function hide(c1,c2,c3,c4,c5) {
-//     document.getElementById(c1).style.display ="none";
-//     document.getElementById(c2).style.display ="none";
-//     document.getElementById(c3).style.display ="none";
-//     document.getElementById(c4).style.display ="none";
-//     document.getElementById(c5).style.display ="none";
-// }
-//  onclick="dis('hide5');"
-//      " hide('hide2','hide3','hide4','hide1','hide6');"
+    function hide(btn1,btn2,btn3,btn4,btn5) {
+        document.getElementById(btn1).style.display ="none";
+        document.getElementById(btn2).style.display ="none";
+        document.getElementById(btn3).style.display ="none";
+        document.getElementById(btn4).style.display ="none";
+        document.getElementById(btn5).style.display ="none";
+    }
+
+
+    // var ul = document.getElementById("skills");
+    //  console.log(ul)
+    // var items = ul.getElementsByTagName("li");
+    // for (var i = 0; i < items.length; ++i) {
+    //     console.log(items[i])
+    // }
+
+
+
+
+ function  display_stars() {
+     var stars = document.getElementsByClassName('stars');
+         for (var i = 0; i<stars.length;i++){
+             stars[i].appendChild('<div>Hello</div>')
+         }
+
+ }
